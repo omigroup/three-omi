2e9c7bc8ddaafd0a4d26e565eab78ed758b26e40
Robert Long <robert@robertlong.me>
2021-07-28 22:18:17 -0700

    v0.0.1

--- vite.config.js
+++ vite.config.js
@@ -48,7 +48,7 @@ export default ({ command, mode }) => {
     return {
       build: {
         lib: {
-          entry: path.resolve(__dirname, "src/three-omi.ts"),
+          entry: path.resolve(__dirname, "src/three-omi.js"),
           name: "OMI",
           formats: ["es", "cjs"],
         },
